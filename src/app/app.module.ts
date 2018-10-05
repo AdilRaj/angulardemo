730cf2d4b8e235a294b1583327770b99de145502
Adil Abdul Khalique <adil.rajput@fwupakistan.com>
2018-10-05 16:51:09 +0500

    Tutorial 3: P1 Routing
    create employeeCreate Component and add it into app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ListEmployeeComponent } from './employees/list-employee.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeeComponent
+    ListEmployeeComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule
