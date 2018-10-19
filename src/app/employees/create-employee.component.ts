3c670eee51eb17362ccec8d324d421124396bec5
Adil Abdul Khalique <adil.rajput@fwupakistan.com>
2018-10-19 17:48:29 +0500

    Tutorial 10: Select List populate from class level property
    we update previous list
    now getting option from class level property
    *ngFor directive use to manipulate dom with iteration
    html property binding is used by [value] angular attribute
    and for showing content we used {{}} interpolation directive

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {Department} from '../model/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -13,6 +14,14 @@ export class CreateEmployeeComponent implements OnInit {
   phoneNumber = '';
   isActive = true;
   department = '3';
+
+  departments: Department[] = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'},
+    {id: 4, name: 'PayRoll'},
+    {id: 5, name: 'Admin'}
+  ];
   constructor() { }
 
   ngOnInit() {
